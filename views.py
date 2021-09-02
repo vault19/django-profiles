b18b5717acfc8fb86521e29fde31d8131adc2edc
xhnilic3 <j.hnilica147@gmail.com>
2021-09-02 10:31:35 +0200

    views - unnecessary imports removed + translation added

--- views.py
+++ views.py
@@ -1,15 +1,26 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.shortcuts import render, redirect
+from django.utils.translation import ugettext_lazy as _
 
-from profiles.models import Address
-from profiles.forms import PasswordChangingForm
+from profiles.models import Address, Profile
+from profiles.forms import PasswordChangingForm, AddressForm
 
 
 # Create your views here.
 
 def my_profile(request):
-    return render(request, 'profiles/my_profile.html', {})
+    profile_info = Profile.objects.get(user=request.user.pk)
+    address = profile_info.address
+    form = AddressForm(request.POST or None, instance=Address(street=address.street, number=address.number,
+                                                              city=address.city, postal_code=address.postal_code,
+                                                              country=address.country), auto_id=1)
+
+    if form.is_valid():
+        form.save()
+
+    print("Profile info: ", profile_info.address.city)
+    return render(request, 'profiles/my_profile.html', {"context": form, "keket": profile_info})
 
 
 # TODO change messages into slovak language
@@ -19,10 +30,10 @@ def change_password(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
-            messages.success(request, 'Vaše heslo bolo úspešne zmenené!')
+            messages.success(request, _('Your password was successfully changed!'))
             return redirect('change_password')
         else:
-            messages.error(request, 'Opravte chybu nižšie, prosím!')
+            messages.error(request, _('Fix the error below, please!'))
     else:
         form = PasswordChangingForm(request.user)
     return render(request, 'profiles/change_password.html', {'form': form})
