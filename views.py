60059349696d44574a316351621d0a10500738ac
xhnilic3 <j.hnilica147@gmail.com>
2021-09-02 17:32:08 +0200

    Added change password functionality in profile (#3)
    Added change password functionality (form, view, template) in user profile.

---
+++ views.py
@@ -0,0 +1,38 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.translation import ugettext_lazy as _
+
+from profiles.models import Address, Profile
+from profiles.forms import PasswordChangingForm, AddressForm
+
+
+# Create your views here.
+@login_required
+def my_profile(request):
+    profile_info = Profile.objects.get(user=request.user.pk)
+    instance = get_object_or_404(Address, id=profile_info.pk)
+    form = AddressForm(request.POST or None, instance=instance)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'profiles/my_profile.html', {"form": form})
+
+
+# TODO change messages into slovak language
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangingForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, _('Your password was successfully changed!'))
+            return redirect('change_password')
+        else:
+            messages.error(request, _('Fix the error below, please!'))
+    else:
+        form = PasswordChangingForm(request.user)
+    return render(request, 'profiles/change_password.html', {'form': form})
